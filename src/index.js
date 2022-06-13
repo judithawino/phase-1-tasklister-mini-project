9ca6adb27237fab6c06ee959a1b3ef9cf1818461
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-13 22:52:51 +0300

    tasklister miniproject deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  typeTaskInput()
 });
+document.addEventListener("submit",function(e){
+  
+  clickSubmit()
+});
+
+function typeTaskInput(){
+  let input=document.getElementById("new-task-description").textContent
+}
+
+function clickSubmit(){
+  e.preventDefault()
+  let newTaskDescription=document.getElementById("new-task-description");
+  let newTask=document.createElement("li");
+  newTask.textContent=newTaskDescription.value;
+  appendNewTask(newTask);
+  e.target.reset();
+
+};
+
+const appendNewTask=task=>{
+  document.getElementById("tasks").appendChild(task);
+}
